50878c420df979f40cf411d7d75f6068f674583f
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 04:28:47 +0700

    Update player.js

--- player.js
+++ player.js
@@ -1,28 +1,47 @@
 const player={
-  x:100,y:100,
-  vx:0,vy:0,
-  w:28,h:28,
-  hp:100,
-  spawnX:100,
-  spawnY:100
+x:100,y:0,w:26,h:30,
+vx:0,vy:0,speed:3,jump:-10,
+onGround:false,hp:100
 };
 
-function updatePlayer(){
-  player.vy+=0.5;
-
-  let nx=player.x+player.vx;
-  let ny=player.y+player.vy;
-
-  if(!isSolid(Math.floor(nx/TILE),Math.floor(player.y/TILE)))
-    player.x=nx;
-
-  if(!isSolid(Math.floor(player.x/TILE),Math.floor(ny/TILE)))
-    player.y=ny;
-  else player.vy=0;
-
-  if(player.hp<=0){
-    player.x=player.spawnX;
-    player.y=player.spawnY;
-    player.hp=100;
-  }
+function spawnPlayer(){
+for(let y=0;y<H;y++){
+ if(isSolid(player.x,y*TILE)){
+  player.y=(y-1)*TILE;break;
+ }
 }
+}
+spawnPlayer();
+
+function updatePlayer(){
+player.vx=0;
+if(input.left) player.vx=-player.speed;
+if(input.right) player.vx=player.speed;
+
+player.vy+=0.5;
+if(player.vy>12) player.vy=12;
+
+if(input.jump&&player.onGround){
+ player.vy=player.jump;
+ player.onGround=false;
+}
+
+let nx=player.x+player.vx;
+if(!isSolid(nx,player.y)&&!isSolid(nx,player.y+player.h))
+ player.x=nx;
+
+let ny=player.y+player.vy;
+if(player.vy>0){
+ if(!isSolid(player.x,ny+player.h)){
+  player.y=ny;player.onGround=false;
+ }else{
+  player.vy=0;player.onGround=true;
+ }
+}else{
+ if(!isSolid(player.x,ny)) player.y=ny;
+ else player.vy=0;
+}
+}
+
+window.player=player;
+window.updatePlayer=updatePlayer;
