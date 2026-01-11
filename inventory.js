472abeab2e8e2aeca67262c0b8c001e2345b17bc
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 06:00:03 +0700

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -6,8 +6,20 @@ function drawHotbar(){
   hb.innerHTML="";
   inventory.forEach(item=>{
     const div=document.createElement("div");
-    if(item.id) div.textContent=item.count;
+    if(item.id) div.textContent=item.id+" x"+item.count;
     hb.appendChild(div);
   });
 }
+function drawInventoryUI(){
+  const invUI=document.getElementById("inventoryUI");
+  invUI.innerHTML="";
+  inventory.forEach((item)=>{
+    const div=document.createElement("div");
+    div.style.width="50px";div.style.height="50px";
+    div.style.border="1px solid black";div.style.display="inline-block";div.style.margin="2px";
+    if(item.id) div.textContent=item.id+" x"+item.count;
+    invUI.appendChild(div);
+  });
+}
 setInterval(drawHotbar,100);
+setInterval(drawInventoryUI,200);
