8313654ebec32ca7cc928faaa7a75b63f0a9a373
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 06:24:05 +0700

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,21 +1,23 @@
-export const input={left:false,right:false,jump:false,attack:false,place:false};
+export const input = { x:0, y:0, jump:false, attack:false, place:false };
 
-// joystick bên trái
+// joystick
 const joy=document.getElementById("joystick");
 let startX,startY;
-joy.addEventListener("touchstart",e=>{
-  startX=e.touches[0].clientX;
-  startY=e.touches[0].clientY;
+joy.addEventListener("touchstart", e => {
+  startX = e.touches[0].clientX;
+  startY = e.touches[0].clientY;
 });
-joy.addEventListener("touchmove",e=>{
-  const dx=e.touches[0].clientX-startX;
-  input.left=dx<-20;
-  input.right=dx>20;
+
+joy.addEventListener("touchmove", e => {
+  const dx = e.touches[0].clientX - startX;
+  const dy = e.touches[0].clientY - startY;
+  
+  // tốc độ
+  input.x = Math.max(-1, Math.min(1, dx/50)); // -1 -> 1
+  input.y = Math.max(-1, Math.min(1, dy/50)); // -1 -> 1
 });
-joy.addEventListener("touchend",e=>{
-  input.left=false;
-  input.right=false;
-});
+
+joy.addEventListener("touchend", e => { input.x=0; input.y=0; });
 
 // nút hành động bên phải
 ["jump","attack","place"].forEach(id=>{
