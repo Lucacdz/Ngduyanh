eca4ea07bd9b39d2e147d0ab785f7bf56f93b024
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 06:30:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ import {input} from "./controls.js";
 const canvas=document.getElementById("game");
 const ctx=canvas.getContext("2d");
 function resize(){canvas.width=innerWidth;canvas.height=innerHeight;}
-resize();addEventListener("resize",resize);
-let camX=0,camY=0;
+resize();window.addEventListener("resize",resize);
+
+let camX=0, camY=0;
 
 function drawWorld(){
   for(let y=0;y<H;y++){
@@ -30,14 +31,15 @@ function loop(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
   updatePlayer(input);
   updateMobs();
-  camX=player.x-canvas.width/2;
-  camY=player.y-canvas.height/2;
+  camX = player.x - canvas.width/2;
+  camY = player.y - canvas.height/2;
   drawWorld();
   drawPlayer(ctx,camX,camY);
   mobs.forEach(m=>drawMob(ctx,m,camX,camY));
   requestAnimationFrame(loop);
 }
 
+// chỉ chạy khi bấm Start
 initMenu(()=>{
   spawnPlayer();
   loop();
