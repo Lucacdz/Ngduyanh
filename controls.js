a90ac05231db7efcc4ea02d8c7b00568c1b40e91
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 06:20:57 +0700

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,15 +1,23 @@
 export const input={left:false,right:false,jump:false,attack:false,place:false};
-// joystick
+
+// joystick bên trái
 const joy=document.getElementById("joystick");
 let startX,startY;
-joy.addEventListener("touchstart",e=>{startX=e.touches[0].clientX; startY=e.touches[0].clientY;});
+joy.addEventListener("touchstart",e=>{
+  startX=e.touches[0].clientX;
+  startY=e.touches[0].clientY;
+});
 joy.addEventListener("touchmove",e=>{
   const dx=e.touches[0].clientX-startX;
   input.left=dx<-20;
   input.right=dx>20;
 });
-joy.addEventListener("touchend",e=>{input.left=false;input.right=false;});
-// nút
+joy.addEventListener("touchend",e=>{
+  input.left=false;
+  input.right=false;
+});
+
+// nút hành động bên phải
 ["jump","attack","place"].forEach(id=>{
   const btn=document.getElementById(id);
   btn.addEventListener("touchstart",()=>input[id]=true);
