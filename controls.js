952f68a62c79443dd40c13b3de044be73891f065
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 04:29:31 +0700

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,9 +1,20 @@
-left.onclick=()=>player.vx=-3;
-right.onclick=()=>player.vx=3;
-jump.onclick=()=>player.vy=-10;
-attack.onclick=()=>{
- mobs.forEach(m=>{
-  if(Math.abs(m.x-player.x)<40 && Math.abs(m.y-player.y)<40)
-    m.hp-=10;
- });
-};
+const input={left:false,right:false,jump:false,attack:false};
+
+["left","right","jump","attack"].forEach(k=>{
+ let b=document.getElementById(k);
+ b.addEventListener("touchstart",e=>{e.preventDefault();input[k]=true});
+ b.addEventListener("touchend",()=>input[k]=false);
+});
+
+document.addEventListener("keydown",e=>{
+ if(e.key=="a")input.left=true;
+ if(e.key=="d")input.right=true;
+ if(e.key=="w")input.jump=true;
+});
+document.addEventListener("keyup",e=>{
+ if(e.key=="a")input.left=false;
+ if(e.key=="d")input.right=false;
+ if(e.key=="w")input.jump=false;
+});
+
+window.input=input;
