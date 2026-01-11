abd319f999689e6e79adb0ceb9a0cae9a90596a9
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 04:34:19 +0700

    Update mobs.js

--- mobs.js
+++ mobs.js
@@ -1,20 +1,36 @@
-const mobs=[];
-function spawnMob(x,y){
-mobs.push({x,y,w:26,h:26,vx:1,vy:0,hp:20});
-}
-spawnMob(300,0);
+const mobs = [];
 
-function updateMobs(){
-mobs.forEach(m=>{
- m.vy+=0.5;
- let nx=m.x+m.vx;
- if(!isSolid(nx,m.y)&&!isSolid(nx,m.y+m.h)) m.x=nx;
- else m.vx*=-1;
-
- let ny=m.y+m.vy;
- if(!isSolid(m.x,ny+m.h)) m.y=ny;
- else m.vy=0;
-});
+function spawnMob(x, y, type = "zombie") {
+  mobs.push({
+    x, y, w: 26, h: 30,
+    vx: Math.random() < .5 ? -1 : 1,
+    vy: 0,
+    hp: 30,
+    maxHp: 30,
+    type
+  });
 }
-window.mobs=mobs;
-window.updateMobs=updateMobs;
+
+spawnMob(300, 0);
+
+function updateMobs() {
+  mobs.forEach(m => {
+    m.vy += 0.5;
+
+    let nx = m.x + m.vx;
+    if (!isSolid(nx, m.y) && !isSolid(nx, m.y + m.h)) m.x = nx;
+    else m.vx *= -1;
+
+    let ny = m.y + m.vy;
+    if (!isSolid(m.x, ny + m.h)) m.y = ny;
+    else m.vy = 0;
+  });
+
+  for (let i = mobs.length - 1; i >= 0; i--) {
+    if (mobs[i].hp <= 0) mobs.splice(i, 1);
+  }
+}
+
+window.mobs = mobs;
+window.spawnMob = spawnMob;
+window.updateMobs = updateMobs;
