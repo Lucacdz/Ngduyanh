ad69fe3703b82c942b6651a5bcfe9c4dd61b119d
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 07:20:00 +0700

    Update tree.js

--- tree.js
+++ tree.js
@@ -19,13 +19,16 @@ export function spawnTrees(){
       for(let y=0;y<H;y++){
         if(world[y][x]!==0){ groundY=y-1; break; }
       }
+      // thân cây
       for(let i=0;i<treeHeight;i++){
         if(groundY-i>0){ world[groundY-i][x]=3; worldHP[groundY-i][x]=4; }
       }
+      // lá: cao hơn gỗ 1 block
+      let leafTop = groundY - treeHeight -1;
       let leafLayers = treeHeight<=3?2:3;
-      for(let ly=-leafLayers;ly<0;ly++){
+      for(let ly=leafTop;ly<groundY-treeHeight+1;ly++){
         for(let lx=-2;lx<=2;lx++){
-          let px=x+lx, py=groundY+ly;
+          let px=x+lx, py=ly;
           if(px>=0 && px<W && py>=0 && py<H && world[py][px]===0){
             world[py][px]=4; worldHP[py][px]=2;
           }
