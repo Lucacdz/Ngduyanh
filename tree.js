f4d90919901a42b6dceca3cfc6bf434bda81bbd9
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 07:17:24 +0700

    Update tree.js

--- tree.js
+++ tree.js
@@ -2,7 +2,6 @@ import {world, worldHP, W, H} from "./world.js";
 import {blocks,getBlockDrop} from "./block.js";
 import {addItem} from "./inventory.js";
 
-// phá block + rơi item
 export function breakBlockDropItem(x,y){
   const id = world[y][x];
   if(id===0) return;
@@ -12,31 +11,23 @@ export function breakBlockDropItem(x,y){
   worldHP[y][x]=0;
 }
 
-// spawn cây ngẫu nhiên
 export function spawnTrees(){
   for(let x=5;x<W;x+=5){
-    if(Math.random()<0.1){ 
+    if(Math.random()<0.1){
       let treeHeight = Math.floor(Math.random()*3)+3;
       let groundY = 0;
       for(let y=0;y<H;y++){
         if(world[y][x]!==0){ groundY=y-1; break; }
       }
-
       for(let i=0;i<treeHeight;i++){
-        if(groundY-i>0){
-          world[groundY-i][x]=3;
-          worldHP[groundY-i][x]=4;
-        }
+        if(groundY-i>0){ world[groundY-i][x]=3; worldHP[groundY-i][x]=4; }
       }
-
       let leafLayers = treeHeight<=3?2:3;
       for(let ly=-leafLayers;ly<0;ly++){
         for(let lx=-2;lx<=2;lx++){
-          let px = x+lx;
-          let py = groundY+ly;
+          let px=x+lx, py=groundY+ly;
           if(px>=0 && px<W && py>=0 && py<H && world[py][px]===0){
-            world[py][px]=4;
-            worldHP[py][px]=2;
+            world[py][px]=4; worldHP[py][px]=2;
           }
         }
       }
