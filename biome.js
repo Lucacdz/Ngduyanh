70755bc66d906d8255b2486451925b695cd8a042
Lucacdz <duyanh4416t@gmail.com>
2026-01-12 07:36:13 +0700

    Update biome.js

--- biome.js
+++ biome.js
@@ -1,25 +1,24 @@
 import {world, worldHP, W, H} from "./world.js";
 import {blocks} from "./block.js";
 
-export const BIOMES = ["mountain","ocean","desert"];
-export const biomeMap = []; // lưu biome từng cột x
+export const BIOMES = ["mountain","ocean","desert","plains"];
+export const biomeMap = [];
 
-// Generate biome map theo X
 export function generateBiomes() {
   for(let x=0;x<W;x++){
-    if(x < W/3) biomeMap[x]="mountain";
-    else if(x < W*2/3) biomeMap[x]="ocean";
-    else biomeMap[x]="desert";
+    if(x<W/4) biomeMap[x]="mountain";
+    else if(x<W/2) biomeMap[x]="ocean";
+    else if(x<W*3/4) biomeMap[x]="desert";
+    else biomeMap[x]="plains";
   }
 }
 
-// Lấy biome theo x
 export function getBiome(x){
   if(x<0||x>=W) return "mountain";
   return biomeMap[x];
 }
 
-// Tạo block theo biome
+// Tạo block cơ bản theo biome
 export function generateBlocks() {
   for(let y=0;y<H;y++){
     world[y]=[];
@@ -27,18 +26,20 @@ export function generateBlocks() {
     for(let x=0;x<W;x++){
       const biome = getBiome(x);
       if(biome==="mountain"){
-        if(y>40) { world[y][x]=1; worldHP[y][x]=blocks[1].hp; } // dirt
+        if(y>40){ world[y][x]=1; worldHP[y][x]=blocks[1].hp; }
+        else if(y===40){ world[y][x]=2; worldHP[y][x]=blocks[2].hp; }
+        else { world[y][x]=0; worldHP[y][x]=0; }
+      } else if(biome==="ocean"){
+        if(y>45){ world[y][x]=5; worldHP[y][x]=blocks[5].hp; }
+        else if(y>=40){ world[y][x]=7; worldHP[y][x]=2; } // water
+        else { world[y][x]=0; worldHP[y][x]=0; }
+      } else if(biome==="desert"){
+        if(y>40){ world[y][x]=5; worldHP[y][x]=blocks[5].hp; }
+        else if(y===40){ world[y][x]=6; worldHP[y][x]=2; } // sand
+        else { world[y][x]=0; worldHP[y][x]=0; }
+      } else if(biome==="plains"){
+        if(y>40){ world[y][x]=1; worldHP[y][x]=blocks[1].hp; } // dirt
         else if(y===40){ world[y][x]=2; worldHP[y][x]=blocks[2].hp; } // grass
-        else { world[y][x]=0; worldHP[y][x]=0; }
-      }
-      else if(biome==="ocean"){
-        if(y>45) { world[y][x]=5; worldHP[y][x]=blocks[5].hp; } // stone
-        else if(y>=40){ world[y][x]=7; worldHP[y][x]=2; } // nước, cần thêm block id=7 "water"
-        else { world[y][x]=0; worldHP[y][x]=0; }
-      }
-      else if(biome==="desert"){
-        if(y>40) { world[y][x]=5; worldHP[y][x]=blocks[5].hp; } // stone dưới
-        else if(y===40){ world[y][x]=6; worldHP[y][x]=2; } // sand, cần thêm block id=6
         else { world[y][x]=0; worldHP[y][x]=0; }
       }
     }
